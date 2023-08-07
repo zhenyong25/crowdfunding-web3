74eab53bfe936b12d75af88e2159e61f3bc4a4d0
zhenyong25 <zhenyong25@gmail.com>
2023-08-07 19:37:09 +0800

    Completed Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// import { Sidebar, Navbar } from './components';
+import { Sidebar } from './components/Sidebar'
 // import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-import { StateContextProvider } from './context';
+// import { StateContextProvider } from './context';
 import App from './App';
 import './index.css';
 
